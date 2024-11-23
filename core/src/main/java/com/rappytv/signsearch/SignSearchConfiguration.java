df1e6ecbeb7098de552d5b18e476e145c00175aa
RappyTV <rappytv@rappytv.com>
2024-11-23 16:15:31 +0100

    Add config option for the settings hotkey

--- core/src/main/java/com/rappytv/signsearch/SignSearchConfiguration.java
+++ core/src/main/java/com/rappytv/signsearch/SignSearchConfiguration.java
@@ -1,14 +1,23 @@
 package com.rappytv.signsearch;
 
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.key.Key;
+import net.labymod.api.client.gui.screen.widget.widgets.input.KeybindWidget.KeyBindSetting;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 public class SignSearchConfiguration extends AddonConfig {
 
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+    @KeyBindSetting
+    private final ConfigProperty<Key> menuHotkey = new ConfigProperty<>(Key.NONE);
+
     @Override
     public ConfigProperty<Boolean> enabled() {
         return enabled;
+    }
+
+    public ConfigProperty<Key> menuHotkey() {
+        return menuHotkey;
     }
 }
