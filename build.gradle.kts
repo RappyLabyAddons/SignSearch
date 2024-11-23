eb029b875c1cef0bbedff327c03b00bdee1e499d
RappyTV <rappytv@rappytv.com>
2024-11-23 16:38:32 +0100

    Add 1.21.3 to supported versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ labyMod {
         displayName = "SignSearch"
         author = "RappyTV"
         description = "Highlight signs which contain a specific text."
-        minecraftVersion = "1.8<1.21.1"
+        minecraftVersion = "1.8<1.21.3"
         version = rootProject.version.toString()
     }
 }
